f04440fde2476ab971f79d1d5d3f45a87d486f54
Daniel Cosmo <danielcosmo.ezio@gmail.com>
2020-05-23 11:43:57 -0300

    Iniciando o projeto

---
+++ src/db/DB.java
@@ -0,0 +1,74 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+	private static Connection conn = null;
+
+	// abrir a conexão
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProps();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return conn;
+	}
+
+	// fechar a conexão
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	// fechar as declações
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	// fechar o ResultSet
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	// carregar as informações de conexão
+	public static Properties loadProps() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+}
